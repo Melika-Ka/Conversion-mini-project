baa5e4dbf91d2200aa527f80639f978094112868
MrColaCandy <MahmoodKhdadady10@gmail.com>
2023-07-05 13:18:08 +0430

    Completed

--- js/app.js
+++ js/app.js
@@ -3,9 +3,28 @@ let resetButton = document.querySelector(".resetButton");
 let changeButton = document.querySelector(".changeButton");
 let result = document.querySelector(".result");
 let converterInput = document.querySelector("#converter");
+let label = document.querySelector("header label");
 let isTempC = true;
 convertButton.addEventListener("click", converting);
+changeButton.addEventListener("click", changeTemp);
+resetButton.addEventListener("click", reset);
+converterInput.addEventListener("keyup", validate);
 
+function validate() {
+  if (converterInput.value != +converterInput.value) {
+    converterInput.value = "";
+  }
+}
+
+function reset() {
+  converterInput.value = "";
+  result.innerHTML = "";
+}
+function changeTemp() {
+  isTempC = !isTempC;
+  var text = isTempC ? "Converter °C to °F" : "Converter °F to °C";
+  label.innerHTML = text;
+}
 function converting() {
   if (isTempC) {
     convertCtoF();
@@ -14,12 +33,12 @@ function converting() {
   }
 }
 function convertCtoF() {
-  let convert = +converterInput.value * 1.8 + 32;
+  let convert = Math.round(+converterInput.value * 1.8 + 32);
   result.innerHTML = +converterInput.value + "°C" + " is: " + convert + "°F";
   converterInput.value = "";
 }
 function convertFtoC() {
-  let convert = (+input.value - 32) / 1.8;
-  result.innerHTML = +converterInput.value + "°C" + " is: " + convert + "°F";
+  let convert = Math.round((+converterInput.value - 32) / 1.8);
+  result.innerHTML = +converterInput.value + "°F" + " is: " + convert + "°C";
   converterInput.value = "";
 }
