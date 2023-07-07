7bd12cb0958971fa18415f58c9eea304f23b3604
soheil bayani <baayani.2.82@gmail.com>
2023-07-07 11:31:28 +0330

    complete reset and change logic

--- js/app.js
+++ js/app.js
@@ -4,7 +4,12 @@ let changeButton = document.querySelector(".changeButton");
 let result = document.querySelector(".result");
 let converterInput = document.querySelector("#converter");
 let isTempC = true;
+let base = document.querySelector(".C");
+let converted = document.querySelector(".F");
+
 convertButton.addEventListener("click", converting);
+resetButton.addEventListener("click", reset);
+changeButton.addEventListener("click", change);
 
 function converting() {
   if (isTempC) {
@@ -19,7 +24,30 @@ function convertCtoF() {
   converterInput.value = "";
 }
 function convertFtoC() {
-  let convert = (+input.value - 32) / 1.8;
-  result.innerHTML = +converterInput.value + "°C" + " is: " + convert + "°F";
+  let convert = (+converterInput.value - 32) / 1.8;
+  result.innerHTML = +converterInput.value + "°F" + " is: " + convert + "°C";
   converterInput.value = "";
+}
+
+function reset() {
+  result.innerHTML = "";
+  converterInput.value = "";
+}
+
+function change() {
+  if (isTempC) {
+    isTempC = false;
+    document.title = `°F to °C`;
+    converterInput.setAttribute("placeholder", "°F");
+    base.innerHTML = "°F";
+    converted.innerHTML = "°C";
+  } else {
+    isTempC = true;
+    document.title = `°C to °F`;
+    converterInput.setAttribute("placeholder", "°C");
+    base.innerHTML = "°C";
+    converted.innerHTML = "°F";
+  }
+
+  reset();
 }
