17e03adc53105b6c336ff25ec8a44c0c24b059b6
zahraabs <zahraabbasiun@gmail.com>
2023-07-08 13:58:36 +0330

    The final change added

--- js/app.js
+++ js/app.js
@@ -3,8 +3,11 @@ let resetButton = document.querySelector(".resetButton");
 let changeButton = document.querySelector(".changeButton");
 let result = document.querySelector(".result");
 let converterInput = document.querySelector("#converter");
+let label = document.querySelector("header label")
 let isTempC = true;
 convertButton.addEventListener("click", converting);
+changeButton.addEventListener("click" , changing);
+resetButton.addEventListener("click" , reseting);
 
 function converting() {
   if (isTempC) {
@@ -19,7 +22,23 @@ function convertCtoF() {
   converterInput.value = "";
 }
 function convertFtoC() {
-  let convert = (+input.value - 32) / 1.8;
-  result.innerHTML = +converterInput.value + "°C" + " is: " + convert + "°F";
+  let convert = (+converterInput.value - 32) / 1.8;
+  result.innerHTML = +converterInput.value + "°F" + " is: " + convert + "°C";
   converterInput.value = "";
+}
+
+function changing() {
+  isTempC = !isTempC;
+  if (isTempC) {
+    label.innerHTML = `  Converter °C to °F`;
+  } else {
+    label.innerHTML = ` Converter °F to °C`;
+    converterInput.setAttribute("placeholder" , "°F");
+  }
+  
+}
+
+function reseting() {
+  converterInput.value = "";
+  result.innerHTML = "";
 }
